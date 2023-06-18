82fec50c6049231c7b6b593e9b6dbdd8d3bd781c
JalilUmar <jalil786.umer@gmail.com>
2023-06-18 14:34:54 +0500

    ok

--- src/app/Assets/PremiumC.tsx
+++ src/app/Assets/PremiumC.tsx
@@ -34,7 +34,7 @@ export default function PremiumC() {
                                 </span>
                             </div>
 
-                            <button className="md:inline-flex flex items-center  bg-gradient-to-t from-[#181e41]  to-[#984D38] md:px-[102px] px-[35px] md:py-3 py-[7px] text-white rounded-xl font-semibold md:mt-5 mt-2 md:text-base text-[12px] " onClick={() => dispatch(addToCart())} ><ImCart className="mr-2 md:text-2xl text-xl " /> Add to Cart</button>
+                            <button className="md:inline-flex flex items-center  bg-gradient-to-t from-[#181e41]  to-[#984D38] md:px-[100px] px-[35px] md:py-3 py-[7px] text-white rounded-xl font-semibold md:mt-5 mt-2 md:text-base text-[12px] " onClick={() => dispatch(addToCart())} ><ImCart className="mr-2 md:text-2xl text-xl " /> Add to Cart</button>
 
                             <Link href='/productDetail' className=" " >
                                 <button className="md:inline-flex flex items-center  bg-gradient-to-t from-[#671ae4] to-[#b75cff]  md:px-[110px] px-[42px] md:py-3 py-[7px] text-white rounded-xl font-semibold md:mt-2 mt-[6px] md:text-base text-[12px] "   ><BsShop className="mr-2 md:text-2xl text-xl " /> Buy Now</button>
